0f951a3f1f9084c4e4ec88a084724a9fd5831af2
Ivan Vinogradov <vinogradovivan548@gmail.com>
2026-05-30 19:24:20 +0300

    Упрощены скрипты, удален мёртвый код

--- certbot/renew.sh
+++ certbot/renew.sh
@@ -3,11 +3,7 @@
 trap "exit 0" TERM INT
 
 while true; do
-    certbot renew \
-        --webroot \
-        -w /var/www/certbot \
-        --quiet \
-        --deploy-hook "touch /var/www/certbot/.reload-nginx"
-    sleep 12h &
+    certbot renew --webroot -w /var/www/certbot --quiet
+    sleep 86400 &
     wait $!
 done
--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -3,39 +3,23 @@ set -e
 
 envsubst '$DOMAIN' < /etc/nginx/nginx-https.conf.template > /etc/nginx/nginx-https.conf
 
-CERT_PATH="/etc/letsencrypt/live/${DOMAIN}/fullchain.pem"
-RELOAD_SIGNAL="/var/www/certbot/.reload-nginx"
-
-if [ -f "$CERT_PATH" ]; then
+if [ -f "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem" ]; then
     cp /etc/nginx/nginx-https.conf /etc/nginx/nginx.conf
-    NEED_UPGRADE=0
 else
     cp /etc/nginx/nginx-http.conf /etc/nginx/nginx.conf
-    NEED_UPGRADE=1
-fi
-
-nginx -g "daemon off;" &
-NGINX_PID=$!
-
-if [ "$NEED_UPGRADE" = "1" ]; then
+    # Switch to HTTPS once the initial certificate is issued
     (
-        echo "Waiting for SSL certificate..."
-        while [ ! -f "$CERT_PATH" ]; do sleep 5; done
-        echo "Certificate found - switching to HTTPS"
+        while [ ! -f "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem" ]; do
+            sleep 5
+        done
         cp /etc/nginx/nginx-https.conf /etc/nginx/nginx.conf
         nginx -s reload
     ) &
 fi
 
-(
-    while true; do
-        sleep 3600
-        if [ -f "$RELOAD_SIGNAL" ]; then
-            rm -f "$RELOAD_SIGNAL"
-            echo "Reload signal detected - reloading nginx"
-            nginx -s reload
-        fi
-    done
-) &
+# Reload daily so renewed certificates are always picked up
+(while true; do sleep 86400; nginx -s reload; done) &
 
+nginx -g "daemon off;" &
+NGINX_PID=$!
 wait $NGINX_PID
